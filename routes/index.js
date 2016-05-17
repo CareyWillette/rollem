71762b8843e6884addda3e0dfc9335aeb96efcdd
CareyWillette <Carey.Willette@gmail.com>
2016-05-17 05:11:50 -0600

    udpate index route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var db = require('monk')(process.env.MONGOLAB_URI || "mongodb://localhost/yahtzoo")
-var topScores = db.get('topScores')
+// var db = require('monk')(process.env.MONGOLAB_URI || "mongodb://localhost/yahtzoo")
+// var topScores = db.get('topScores')
 
 
 /* GET home page. */
@@ -9,23 +9,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/leaderboard', function(req, res){
-
-  topScores.insert({
-    name: req.body.name[1],
-    score: Number(req.body.score),
-    creditCard: req.body.creditCard[1],
-    SSN: req.body.SSN[1]
-  }, function(err, data){
-    topScores.find({}, {limit : 10, sort: {score:-1}},
-      function(err, data){
-        res.render('leaderboard', {data: data}, function(err, html){
-          res.send(html)
-        });
-    })
-  })
-
-
-});
+// router.post('/leaderboard', function(req, res){
+//
+//   topScores.insert({
+//     name: req.body.name[1],
+//     score: Number(req.body.score),
+//     creditCard: req.body.creditCard[1],
+//     SSN: req.body.SSN[1]
+//   }, function(err, data){
+//     topScores.find({}, {limit : 10, sort: {score:-1}},
+//       function(err, data){
+//         res.render('leaderboard', {data: data}, function(err, html){
+//           res.send(html)
+//         });
+//     })
+//   })
+//
+//
+// });
 
 module.exports = router;
