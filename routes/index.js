de043452b8a91a265978310f3303a6eae4159c47
CareyWillette <Carey.Willette@gmail.com>
2016-05-17 04:32:52 -0600

    initial commit. should have commited earlier since the code base is complete

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var db = require('monk')(process.env.MONGOLAB_URI || "mongodb://localhost/yahtzoo")
+var topScores = db.get('topScores')
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/leaderboard', function(req, res){
+
+  topScores.insert({
+    name: req.body.name[1],
+    score: Number(req.body.score),
+    creditCard: req.body.creditCard[1],
+    SSN: req.body.SSN[1]
+  }, function(err, data){
+    topScores.find({}, {limit : 10, sort: {score:-1}},
+      function(err, data){
+        res.render('leaderboard', {data: data}, function(err, html){
+          res.send(html)
+        });
+    })
+  })
+
+
+})
+
+module.exports = router;
